a524a2e99170ba15271260db2d730e547c1f8f1d
mulukenrock <muluken@cropconex.com>
2024-08-23 18:37:58 +0300

    :sparkles: added a reusable current-time-fetcher function

---
+++ src/utils/dateUtils.js
@@ -0,0 +1,12 @@
+// a global current date-time fetching function
+export async function fetchDate() {
+  try {
+    const response = await fetch("http://worldtimeapi.org/api/ip");
+    const data = await response.json();
+    const date = new Date(data.datetime);
+    return date;
+  } catch (error) {
+    console.error("Error fetching the current date: ", error);
+    return new Date(); // Fallback to the client’s local date
+  }
+}
